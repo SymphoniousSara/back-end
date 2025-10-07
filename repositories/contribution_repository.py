07b93e9fa97d8da546bee8afc85b5d42b18d81d8
SymphoniousSara <sara.apostolovska@symphony.is>
2025-10-07 15:16:39 +0200

    Defined unique functions for repositories.

--- repositories/contribution_repository.py
+++ repositories/contribution_repository.py
@@ -1,6 +1,6 @@
-from typing import Generic, Optional, TypeVar, Type, List
+from typing import Generic, Optional, TypeVar, Type, List, Any
 from uuid import UUID
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, joinedload
 from db.database import Base
 
 from models.contributions import Contribution
@@ -9,3 +9,17 @@ from repositories.base import BaseRepository
 class UserRepository(BaseRepository[Contribution]):
     def __init__(self, db: Session):
         super(BaseRepository, self).__init__(Contribution,db)
+
+    def get_by_birthday_id(self, birthday_id: UUID) -> list[type[Contribution]]:
+        # Getting all the contributors by birthday_id
+        return self.db.query(Contribution).filter(
+            Contribution.birthday_id == birthday_id
+        ).options(
+            joinedload(Contribution.contributions)
+        ).all()
+    def get_by_contributor_id(self, contributor_id: UUID) -> list[type[Contribution]]:
+        return self.db.query(Contribution).filter(
+            Contribution.contributor_id == contributor_id
+        ).options(
+            joinedload(Contribution.birthday).joinedload('user_id')
+        ).all()
